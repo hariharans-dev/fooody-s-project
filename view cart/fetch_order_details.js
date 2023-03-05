1c221e3e4fb55e7114dd4f628fee9fbbdef494a6
Hariharan <hariharanjayachandran04@gmail.com>
2023-03-05 22:56:27 +0530

    h

--- view cart/fetch_order_details.js
+++ view cart/fetch_order_details.js
@@ -1,16 +1,16 @@
 var cart = angular.module('cartpage', []);
 cart.controller('data-fetch', function ($scope, $http) {
-    $http.get('data.json')
+    $http.get('/menu selection/data.json')
         .then(function (response) {
+            console.log(response.data[0].count);
             for (let index = 0; index < response.data.length; index++) {
-                console.log(response.data[index].count);
+                
                 if (response.data[index].count > 0) {
                     $scope.name = response.data[index].name;
                     $scope.count = response.data[index].count;
                 }
                 else {
-                    $scope.name = "";
-                    $scope.count = "";
+                    continue;
                 }
             }
             
