3ee0745205d7eb560fbec34ad9e9a55d4a6f3646
Hariharan <hariharanjayachandran04@gmail.com>
2023-03-06 20:46:34 +0530

    view cart

--- view cart/fetch_order_details.js
+++ view cart/fetch_order_details.js
@@ -1,18 +1,12 @@
-var cart = angular.module('cartpage', []);
-cart.controller('data-fetch', function ($scope, $http) {
-    $http.get('C:/Users/shari/Downloads/data.json')
-        .then(function (response) {
-            console.log(response.data[0].count);
-            for (let index = 0; index < response.data.length; index++) {
-                
-                if (response.data[index].count > 0) {
-                    $scope.name = response.data[index].name;
-                    $scope.count = response.data[index].count;
-                }
-                else {
-                    continue;
-                }
-            }
-            
-        });
+$.ajax({
+    url: "",
+    dataType: "json",
+    success: function (data) {
+        // data is a JavaScript object representing the parsed JSON
+        console.log(data);
+    },
+    error: function (xhr, textStatus, errorThrown) {
+        console.log("Error retrieving JSON data:", errorThrown);
+    }
 });
+
