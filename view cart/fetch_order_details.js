5f16b83189516ad7ec0c1eeea84aa75472d8e5bd
hari <shariharan182003@gmail.com>
2023-03-07 09:51:51 +0530

    near final 2

--- view cart/fetch_order_details.js
+++ view cart/fetch_order_details.js
@@ -1,18 +1,44 @@
-var cart = angular.module('cartpage', []);
-cart.controller('data-fetch', function ($scope, $http) {
-    $http.get('C:/Users/shari/Downloads/data.json')
-        .then(function (response) {
-            console.log(response.data[0].count);
-            for (let index = 0; index < response.data.length; index++) {
-                
-                if (response.data[index].count > 0) {
-                    $scope.name = response.data[index].name;
-                    $scope.count = response.data[index].count;
-                }
-                else {
-                    continue;
+let pdata;
+
+$.ajax({
+    url: "data.json",
+    dataType: "json",
+    success: function (data) {
+        pdata = data;
+        let checkEmpty=0;
+        for (let index = 0; index < pdata.length; index++) {
+            if (pdata[index].count != 0) {
+                checkEmpty = 1;
+                break;
+            }
+        }
+        if (checkEmpty != 0) {
+            var tot = 0;
+            $("#table").append("<tr><th>Item</th><th>Price</th><th>Quantity</th><th>Rate</th></tr>")
+            for (let index = 0; index < pdata.length; index++) {
+                if (pdata[index].count != 0) {
+                    var img = "<img class='item-image' height='100' width='100' src=" + pdata[index].img + ">";
+                    var price = "<p>" + pdata[index].price + "</p>";
+                    var quantity = "<p>" + pdata[index].count + "</p>";
+                    var rate = "<p>" + pdata[index].price * pdata[index].count + "</p>";
+                    tot = tot + (pdata[index].price * pdata[index].count);
+                    $("#table").append("<tr><td class='item'>" + img + "<p class='name'>" + pdata[index].name.toUpperCase() + "</p>" + "</td>" + "<td>" + price + "</td>" + "<td>" + quantity + "</td>" + "<td>" + rate + "</td>" + "</tr>");
                 }
             }
-            
-        });
+            $("#table").append("<tr><td colspan='4' class='total'>Total: "+tot+".00</td></tr>")
+        }
+        else {
+            $("#table").hide();
+            $("#main-area").append("<img src='/resources/emptycart.png' class='empty-cart'>");
+            $("#main-area").append("<p class='ecart-p'>Your Cart Is Empty 😞  </p>")
+            $(".ecart-p").append("<a href='/menu selection/menu.html'><button class='glow-on-hover' type='button'>Menu</button></a>");
+        }
+    },
+    error: function (xhr, textStatus, errorThrown) {
+        console.log("Error retrieving JSON data:", errorThrown);
+    }
 });
+
+
+
+
