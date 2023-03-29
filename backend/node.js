2429aaae89367931ba5e4821366b5041253be24a
hari <shariharan182003@gmail.com>
2023-03-29 17:33:58 +0530

    login page is fully completed

--- backend/node.js
+++ backend/node.js
@@ -14,6 +14,13 @@ var userSchema=new mongoose.Schema({
 
 var user=mongoose.model("user",userSchema)
 
+mongoose.connect('mongodb://127.0.0.1:27017/foodlogin',{useNewUrlParser: true, useUnifiedTopology: true});
+const db=mongoose.connection;
+db.on('error',(err)=>{throw err})
+db.once('open',()=>{
+    console.log('db connected')
+})
+
 
 app.listen(8000,(req,res)=>{
     console.log('server is up')
@@ -38,13 +45,6 @@ app.get('/signup',(req,res)=>{
 
     const obj=req.query
     console.log('signup page details is processed')
-
-    mongoose.connect('mongodb://127.0.0.1:27017/foodlogin',{useNewUrlParser: true, useUnifiedTopology: true});
-    const db=mongoose.connection;
-    db.on('error',(err)=>{throw err})
-    db.once('open',()=>{
-        console.log('db connected')
-    })
 
 
     user.create({
@@ -92,5 +92,57 @@ app.get('/signup',(req,res)=>{
 // })
 
 app.get('/loginform',(req,res)=>{
-    res.send(req.query['userid']+" "+req.query['password'])
+
+    const username=req.query['user']
+    const password=req.query['password']
+    var found=0
+    var index
+    // res.send(username+" "+password)
+    user.find({},{_id:0,username:1,password:1})
+    .then((result)=>{
+        for(let i=0;i<result.length;i++){
+            if(username==result[i]['username']){
+                found=1
+                index=i
+                break
+            }
+        }
+        if((found==1)&&(password==result[index]['password'])){
+            console.log('can login')
+
+            const staticpath3 = path.join(__dirname,"../menu/public")
+            app.use(express.static(staticpath3))
+            res.sendFile(path.join(__dirname,"../menu/public/menu.html"))
+        }
+        else if(found==1){
+            console.log('invalid password')
+
+            const staticpath1 = path.join(__dirname,"../loginpage/public")
+            app.use(express.static(staticpath1))
+
+            const html = fs.readFileSync(path.join(__dirname,"../loginpage/public/login.html"), 'utf-8')
+            const $ = cheerio.load(html)
+            const pTag = $('#replace')
+            pTag.text('Invalid Password!')
+            fs.writeFileSync(path.join(__dirname,"../loginpage/public/login.html"), $.html());
+
+            res.sendFile(path.join(__dirname,"../loginpage/public/login.html"))
+        }
+        else{
+            console.log('invalid username')
+
+            const staticpath1 = path.join(__dirname,"../loginpage/public")
+            app.use(express.static(staticpath1))
+
+            const html = fs.readFileSync(path.join(__dirname,"../loginpage/public/login.html"), 'utf-8')
+            const $ = cheerio.load(html)
+            const pTag = $('#replace')
+            pTag.text('Invalid User ID!')
+            fs.writeFileSync(path.join(__dirname,"../loginpage/public/login.html"), $.html());
+
+            res.sendFile(path.join(__dirname,"../loginpage/public/login.html"))
+        }
+    })
+    .catch(err=>{console.log('error in finding')})
+
 })
