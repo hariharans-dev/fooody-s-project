54f9cd79b79dc4ff4a735dce8db42249fbd527ae
hari <shariharan182003@gmail.com>
2023-03-30 11:37:35 +0530

    some node implementations

--- backend/node.js
+++ backend/node.js
@@ -14,8 +14,8 @@ var userSchema=new mongoose.Schema({
 
 var user=mongoose.model("user",userSchema)
 
-mongoose.connect('mongodb://127.0.0.1:27017/foodlogin',{useNewUrlParser: true, useUnifiedTopology: true});
-const db=mongoose.connection;
+mongoose.connect('mongodb://127.0.0.1:27017/foodlogin',{useNewUrlParser: true, useUnifiedTopology: true})
+const db=mongoose.connection
 db.on('error',(err)=>{throw err})
 db.once('open',()=>{
     console.log('db connected')
@@ -82,13 +82,6 @@ app.get('/signup',(req,res)=>{
 })
 
 
-// app.get('/',(req,res)=>{
-//     const staticpath3 = path.join(__dirname,"../viewcart/public")
-//     app.use(express.static(staticpath3))
-//     res.sendFile(path.join(__dirname,"../viewcart/public/cart.html"))
-// })
-
-
 app.get('/loginform',(req,res)=>{
 
     const username=req.query['user']
@@ -152,3 +145,58 @@ app.get('/loginform',(req,res)=>{
     .catch(err=>{console.log('error in finding')})
 
 })
+
+var obj
+
+app.get('/menu',(req,res)=>{
+    console.log('menu page')
+    obj=req.query
+
+    const staticpath1 = path.join(__dirname,"../viewcart/public")
+    app.use(express.static(staticpath1))
+
+
+
+    // function insertion_data(data) {
+    //     const pdata = data
+    //     let checkEmpty=0
+
+    //     const html = fs.readFileSync(path.join(__dirname,"../viewcart/public/cart.html"), 'utf-8')
+    //     const $ = cheerio.load(html)
+
+    //     for (let index = 0; index < pdata.length; index++) {
+    //         if (pdata[index].count != 0) {
+    //             checkEmpty = 1;
+    //             break;
+    //         }
+    //     }
+    //     if (checkEmpty != 0) {
+    //         var tot = 0;
+    //         $("#table").append("<tr><th>Item</th><th>Price</th><th>Quantity</th><th>Rate</th></tr>")
+    //         for (let index = 0; index < pdata.length; index++) {
+    //             if (pdata[index].count != 0) {
+    //                 var img = "<img class='item-image img-circle' src=" + pdata[index].img + ">";
+    //                 var price = "<p>" + pdata[index].price + "</p>";
+    //                 var quantity = "<p>" + pdata[index].count + "</p>";
+    //                 var rate = "<p>" + pdata[index].price * pdata[index].count + "</p>";
+    //                 tot = tot + (pdata[index].price * pdata[index].count);
+    //                 $("#table").append("<tr><td class='item'>" + img + "<p class='name'>" + pdata[index].name.toUpperCase() + "</p>" + "</td>" + "<td>" + price + "</td>" + "<td>" + quantity + "</td>" + "<td>" + rate + "</td>" + "</tr>");
+    //             }
+    //         }
+    //         $("#table").append("<tr><td colspan='4' class='total'>Total: "+tot+".00</td></tr>")
+    //     }
+    //     else {
+    //         $("#table").hide();
+    //         $("#main-area").append("<img src='/resources/emptycart.png' class='empty-cart'>");
+    //         $("#main-area").append("<p class='ecart-p'>Your Cart Is Empty 😞  </p>")
+    //         $(".ecart-p").append("<a href='/menu selection/menu.html'><button class='glow-on-hover' type='button'>Menu</button></a>");
+    //     }
+    // }
+    // insertion_data(obj)
+
+
+
+    fs.writeFileSync(path.join(__dirname,"../viewcart/public/cart.html"), $.html());
+    res.sendFile(path.join(__dirname,"../viewcart/public/cart.html"))
+})
+
