7e8c27adf6e8b5aa40e5a12fa1aa84bee9b5e4e9
hari <shariharan182003@gmail.com>
2023-04-10 10:06:49 +0530

    address draft

--- backend/node.js
+++ backend/node.js
@@ -416,25 +416,19 @@ app.get("/menufile", (req, res) => {
 });
 
 app.get("/addresspage", (req, res) => {
-  console.log('address is loaded')
-  const staticpath9=path.join(__dirname,"../address/public")
-  app.use(express.static(staticpath9))
-  const html = fs.readFileSync(path.join(__dirname, "../address/public/address.html"),"utf-8");
-  const $ = cheerio.load(html);
-  $("#replace").text('')
-
-  let date = DateTime.local();
-  let data
-  
+  console.log('address is loaded')  
 
   var addressuser=mongoose.model('addressuser',userSchema)
   addressuser.findOne({username:customer},{_id:0})
   .then((result)=>{
     var address=result['address']
-    data=address
+    const staticpath9=path.join(__dirname,"../address/public")
+    app.use(express.static(staticpath9))
+    const html = fs.readFileSync(path.join(__dirname, "../address/public/address.html"),"utf-8");
+    const $ = cheerio.load(html);
+    $("#replace").text("Your address: "+address)
+    $("#customer").text("Thanks for ordering Mr./Mrs. "+customer)
+    fs.writeFileSync(path.join(__dirname, "../address/public/address.html"),$.html());
   })
-  console.log(data)
-  $("#replace").append(data)
-  fs.writeFileSync(path.join(__dirname, "../address/public/address.html"),$.html());
   res.sendFile(path.join(__dirname, "../address/public/address.html"));
 });
